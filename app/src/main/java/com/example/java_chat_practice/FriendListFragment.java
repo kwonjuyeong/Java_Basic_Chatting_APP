f53860e442075671ea1280cc6afd9e653b5ca4e8
kwonjuyoung <20161527@vision.hoseo.edu>
2021-11-17 00:46:22 +0900

    java friendlist practice

---
+++ app/src/main/java/com/example/java_chat_practice/FriendListFragment.java
@@ -0,0 +1,162 @@
+package com.example.java_chat_practice;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link FriendListFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class FriendListFragment extends Fragment {
+
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+    private ArrayList<FriendInfo> friendInfoArrayList = new ArrayList<FriendInfo>();
+    private RecyclerView.Adapter adapter;
+
+    public FriendListFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment FriendListFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static FriendListFragment newInstance(String param1, String param2) {
+        FriendListFragment fragment = new FriendListFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+
+         setHasOptionsMenu(true);
+    }
+
+    @Override
+    public void onCreateOptionsMenu(@NonNull Menu menu, @NonNull MenuInflater inflater) {
+        super.onCreateOptionsMenu(menu, inflater);
+        inflater.inflate(R.menu.friendlist_menu_layout,menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.menu_friend_add:
+                friendInfoArrayList.add(new FriendInfo("new friend","new email"));
+                adapter.notifyDataSetChanged();
+                break;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View rootView = inflater.inflate(R.layout.fragment_friend_list, container, false);
+
+        RecyclerView recyclerView = rootView.findViewById(R.id.recyclerView);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        adapter = new RecyclerView.Adapter() {
+            @NonNull
+            @Override
+            public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+                View itemView = LayoutInflater.from(getContext()).inflate(R.layout.recyclerview_itemview_layout, parent, false);
+                RecyclerView.ViewHolder viewHolder = new MyViewHolder(itemView);
+                return viewHolder;
+            }
+
+            @Override
+            public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+                MyViewHolder viewHolder = (MyViewHolder) holder;
+
+                viewHolder.setName(friendInfoArrayList.get(position).getName());
+                viewHolder.setEmail(friendInfoArrayList.get(position).getEmail());
+            }
+
+            @Override
+            public int getItemCount() {
+                return friendInfoArrayList.size();
+            }
+        };
+        recyclerView.setAdapter(adapter);
+
+        return rootView;
+    }
+
+    private static class MyViewHolder extends RecyclerView.ViewHolder {
+
+        private final TextView textViewName;
+        private final TextView textViewEmail;
+
+        public MyViewHolder(View itemView) {
+            super(itemView);
+
+            textViewName = itemView.findViewById(R.id.textViewName);
+            textViewEmail = itemView.findViewById(R.id.textViewEmail);
+        }
+
+        public void setName(String name) {
+            textViewName.setText(name);
+        }
+
+        public void setEmail(String email) {
+            textViewEmail.setText(email);
+        }
+    }
+
+    private class FriendInfo {
+        private String name;
+        private String email;
+
+        public FriendInfo(String name, String email) {
+            this.name = name;
+            this.email = email;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public String getEmail() {
+            return email;
+        }
+    }
+}
--- app/src/main/java/com/example/java_chat_practice/MainActivity.java
+++ app/src/main/java/com/example/java_chat_practice/MainActivity.java
@@ -1,12 +1,19 @@
 package com.example.java_chat_practice;
+
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+import androidx.viewpager2.widget.ViewPager2;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
-import android.widget.TextView;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
+import com.firebase.ui.auth.viewmodel.AuthViewModelBase;
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
@@ -14,51 +21,124 @@ import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.gms.common.api.ApiException;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.android.material.tabs.TabLayout;
+import com.google.android.material.tabs.TabLayoutMediator;
 import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.auth.GoogleAuthProvider;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 
 public class MainActivity extends AppCompatActivity {
 
-    private static final int RC_SIGN_IN = 0;
-    private static final String TAG = "ChattingApp123";
-    private FirebaseAuth mAuth = FirebaseAuth.getInstance();
-    private FirebaseUser currentUser;
+
+    private static final int RC_SIGN_IN = 1;
+    private static final String TAG = "ChattingApp";
+    private static final String[] TAB_NAMES = {"Friends", "Chat List"};
     private GoogleSignInClient mGoogleSignInClient;
-    private TextView textView;
+    public static final String REQ_ID ="955456048321-m8lhve199eqmcr8abj9gkchqebb59f5c.apps.googleusercontent.com";
+    private FirebaseAuth mAuth = FirebaseAuth.getInstance();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        textView = findViewById(R.id.textView);
 
-        signIn();
-    }
+        ViewPager2 viewPager = findViewById(R.id.viewPager);
+        TabLayout tabLayout = findViewById(R.id.tabLayout);
 
-    @Override
-    protected void onResume() {
-        super.onResume();
-        if(currentUser == null) {
-            signIn();
-        }
-        else{
-            textView.setText("hello" + currentUser.getDisplayName());
-        }
-    }
+        viewPager.setAdapter(new FragmentStateAdapter(this) {
+            @NonNull
+            @Override
+            public Fragment createFragment(int position) {
+                switch (position){
+                    case 0:
+                        return new FriendListFragment();
+                    case 1:
+                        return new ChattingListFragment();
+                }
+                return null;
+            }
 
+            @Override
+            public int getItemCount() {
+                return TAB_NAMES.length;
+            }
+        });
 
-    private void signIn() {
+        new TabLayoutMediator(tabLayout, viewPager, new TabLayoutMediator.TabConfigurationStrategy() {
+            @Override
+            public void onConfigureTab(@NonNull TabLayout.Tab tab, int position) {
+                tab.setText(TAB_NAMES[position]);
+            }
+        }).attach();
+
+        getSupportActionBar().setTitle(TAB_NAMES[0]);
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+
+                getSupportActionBar().setTitle(TAB_NAMES[tab.getPosition()]);
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+
         GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-                .requestIdToken("375254490249-ogjqlpkljok0fpe9r0fbn8r8ctoojoes.apps.googleusercontent.com")
+                .requestIdToken(REQ_ID)
                 .requestEmail()
                 .build();
 
         mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
 
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.main_menu_layout, menu);
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+
+            case R.id.menu_signout:
+                mAuth.signOut();
+                mGoogleSignInClient.revokeAccess();
+                signIn();
+                break;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        if(mAuth.getCurrentUser()==null){
+            signIn();
+        }
+        updateFriendList(mAuth.getCurrentUser());
+    }
+
+    private void signIn() {
         Intent signInIntent = mGoogleSignInClient.getSignInIntent();
         startActivityForResult(signInIntent, RC_SIGN_IN);
     }
@@ -91,19 +171,23 @@ public class MainActivity extends AppCompatActivity {
                         if (task.isSuccessful()) {
                             // Sign in success, update UI with the signed-in user's information
                             Log.d(TAG, "signInWithCredential:success");
-                            currentUser = mAuth.getCurrentUser();
-                            updateUI(currentUser);
+                            FirebaseUser user = mAuth.getCurrentUser();
+
+                            updateFriendList(user);
+
                         } else {
                             // If sign in fails, display a message to the user.
                             Log.w(TAG, "signInWithCredential:failure", task.getException());
-                            updateUI(null);
                         }
                     }
                 });
     }
 
-    private void updateUI(FirebaseUser user) {
+    private void updateFriendList(FirebaseUser user) {
+        if(user == null) return;
 
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference ref_friendlistlist = database.getReference("friendlist").child(user.getUid());
+        ref_friendlistlist.setValue("hello");
     }
-
 }
